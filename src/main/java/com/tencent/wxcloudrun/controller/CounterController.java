eaaa39760ddd5ee388de5fa7da6c1e3828cefc4d
cenxiaowei <cxw153410904@qq.com>
2022-08-26 11:56:56 +0800

    test

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -7,12 +7,15 @@ import com.tencent.wxcloudrun.dto.CounterRequest;
 import com.tencent.wxcloudrun.model.Counter;
 import com.tencent.wxcloudrun.service.CounterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.List;
 
@@ -25,6 +28,99 @@ public class CounterController {
 
   final CounterService counterService;
   final Logger logger;
+
+  /**
+   * 验证签名
+   *
+   * @param signature
+   * @param timestamp
+   * @param nonce
+   * @return
+   */
+  public static boolean checkSignature(String signature, String timestamp, String nonce) {
+
+    // 将token、timestamp、nonce三个参数进行字典排序
+    String[] arr = new String[] {"yanjiaoshou", timestamp, nonce};
+    Arrays.sort(arr);
+
+    // 将三个参数字符串拼接成一个字符串
+    StringBuilder content = new StringBuilder();
+    for (int i = 0; i < arr.length; i++) {
+      content.append(arr[i]);
+    }
+    try {
+      //获取加密工具
+      MessageDigest md = MessageDigest.getInstance("SHA-1");
+      // 对拼接好的字符串进行sha1加密
+      byte[] digest = md.digest(content.toString().getBytes());
+      String tmpStr = byteToStr(digest);
+      //获得加密后的字符串与signature对比
+      return tmpStr != null ? tmpStr.equals(signature.toUpperCase()): false;
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  private static String byteToStr(byte[] byteArray) {
+    String strDigest = "";
+    for (int i = 0; i < byteArray.length; i++) {
+      strDigest += byteToHexStr(byteArray[i]);
+    }
+    return strDigest;
+  }
+
+  private static String byteToHexStr(byte mByte) {
+    char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+            'B', 'C', 'D', 'E', 'F' };
+    char[] tempArr = new char[2];
+    tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+    tempArr[1] = Digit[mByte & 0X0F];
+    String s = new String(tempArr);
+    return s;
+  }
+
+
+
+
+  @GetMapping(value = "/wxCallback")
+  @ResponseBody
+  public void openPushMsg(HttpServletRequest request, HttpServletResponse response) {
+
+    // 微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+    String signature = request.getParameter("signature");
+    // 时间戳
+    String timestamp = request.getParameter("timestamp");
+    // 随机数
+    String nonce = request.getParameter("nonce");
+    // 随机字符串
+    String echostr = request.getParameter("echostr");
+
+    PrintWriter out = null;
+    try {
+      out = response.getWriter();
+      if (checkSignature(signature, timestamp, nonce)) {
+        out.print(echostr);
+        out.flush();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      out.close();
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
 
   public CounterController(@Autowired CounterService counterService) {
     this.counterService = counterService;
@@ -79,5 +175,16 @@ public class CounterController {
       return ApiResponse.error("参数action错误");
     }
   }
-  
+
+
+
+
+
+
+
+
+
+
+
+
 }
